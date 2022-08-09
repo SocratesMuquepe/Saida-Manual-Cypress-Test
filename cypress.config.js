613c1be544fa304ca95621ee24b31d3e978481ab
Sócrates Muquepe <socrates.muquepe@minfin.gov.ao>
2022-08-09 15:21:11 +0100

    Update Cypress Version

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/report/mochawesome-report',
+    overwrite: true,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss',
+  },
+  projectId: 'mgmm75',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+}
